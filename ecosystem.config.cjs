42b8543ab97def299fb9f75be74f5c5d2f739ab7
Atrox493 <2577632081@qq.com>
2026-08-28 01:09:16 +0800

    ci: add Tencent Cloud automatic deployment

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,1 +1,17 @@
-module.exports={apps:[{name:'personal-blog',script:'.output/server/index.mjs',instances:1,exec_mode:'fork',env:{NODE_ENV:'production',PORT:3000,HOST:'127.0.0.1'},max_memory_restart:'512M',time:true}]}
+module.exports = {
+  apps: [{
+    name: 'personal-blog',
+    cwd: __dirname,
+    script: '.output/server/index.mjs',
+    node_args: '--env-file=.env',
+    instances: 1,
+    exec_mode: 'fork',
+    env: {
+      NODE_ENV: 'production',
+      PORT: 3000,
+      HOST: '127.0.0.1',
+    },
+    max_memory_restart: '512M',
+    time: true,
+  }],
+}
